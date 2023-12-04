8bb0181fadf3577503fa054b2791b2899da4b6a6
Huy Tran <continuousml@gmail.com>
2023-12-04 14:19:03 -0500

    initial code base

---
+++ src/utils/.ipynb_checkpoints/utils-checkpoint.py
@@ -0,0 +1,40 @@
+import torch
+
+def generate_rand_projections(dim, n_projs=1000):
+    projs = torch.randn(n_projs, dim)
+    return projs / torch.norm(projs, p=2, dim=1, keepdim=True)
+
+def sample_weight(kappa, dim):
+    kappa = torch.tensor(kappa, dtype=torch.float32)
+    dim = torch.tensor(dim, dtype=torch.float32)
+    b = dim / (torch.sqrt(torch.tensor(4.0) * kappa**2 + dim**2) + 2 * kappa)
+    x = (1 - b) / (1 + b)
+    c = kappa * x + dim * torch.log(1 - x**2)
+    while True:
+        z = torch.distributions.beta.Beta((dim - 1) / 2, (dim - 1) / 2).sample()
+        w = (1 - (1 + b) * z) / (1 - (1 - b) * z)
+        u = torch.rand(1)
+        if kappa * w + dim * torch.log(1 - x * w) - c >= torch.log(u):
+            return w
+
+def get_orthonormal_basis(mu):
+    if torch.allclose(mu, torch.tensor([1.0, 0.0, 0.0])):
+        return torch.eye(3)
+    else:
+        v = torch.tensor([1.0, 0.0, 0.0]) - torch.dot(mu, torch.tensor([1.0, 0.0, 0.0])) * mu
+        v = v / v.norm(p=2)
+        w = torch.cross(mu, v)
+        return torch.stack([mu, v, w])
+
+def sample_vmf(mu, kappa, num_samples):
+    mu = torch.tensor(mu, dtype=torch.float32)
+    dim = mu.size(0)
+    mu = mu / mu.norm(p=2)
+    result = torch.zeros(num_samples, dim)
+    for n in range(num_samples):
+        w = sample_weight(kappa, dim)
+        v = torch.randn(dim - 1)
+        v = v / v.norm(p=2)
+        result[n, :] = torch.cat([torch.tensor([w]), torch.sqrt(1 - w**2) * v])
+    U = get_orthonormal_basis(mu)
+    return result @ U.T
---
+++ src/utils/s3w_utils.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+def get_stereo_projection(x):
+    d = x.shape[-1] - 1
+    numerator = x[..., :d]
+    denominator = 1 - x[..., d]
+    near_pole = np.isclose(denominator, 0)
+    projection = np.full_like(x[..., :d], np.inf) 
+    projection[~near_pole] = numerator[~near_pole] / denominator[~near_pole, np.newaxis]
+    return torch.tensor(projection)
+
+class Phi(nn.Module):
+    def __init__(self, size):
+        super(Phi, self).__init__()
+        self.size = size
+        self.net = nn.Sequential(nn.Linear(self.size, self.size))
+    def forward(self, x):
+        xhat = self.net(x)
+        return torch.cat((x,xhat),dim=-1)
