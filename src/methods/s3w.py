4096cd1c1cc293fa6e52b704ee25ceb6a1418847
Huy Tran <continuousml@gmail.com>
2024-01-05 21:51:00 -0500

    pseudo np

--- src/methods/s3w.py
+++ src/methods/s3w.py
@@ -3,88 +3,58 @@ import torch.nn as nn
 import torch.optim as optim
 
 from utils.utils import generate_rand_projs
-from utils.s3w_utils import get_stereo_proj_torch, Phi
+from utils.s3w_utils import get_stereo_proj_torch, Phi, calc_rot, rotate, hStar, make_virtual_grid, find_pnp
 
 from tqdm import tqdm
 import time
 
     
-def s3wd0(samples_1, samples_2, p, h=None, n_projs=1000, device='cpu'):
-    if h is None: h=nn.Identity()
-    samples_1_sp = get_stereo_proj_torch(samples_1).to(device)
-    samples_2_sp = get_stereo_proj_torch(samples_2).to(device)
-
-    s1_h = h(samples_1_sp).double()
-    s2_h = h(samples_2_sp).double()
+def s3wd(X, Y, p, h=None, n_projs=1000, device='cpu', random_pnp=True):
+    if h is None: h = hStar()
     
+    X_ = X.to(device)
+    Y_ = Y.to(device)
+    
+    if random_pnp:
+        pnp = torch.rand(3,device=device)
+        pnp[-1] = 0
+        pnp /= torch.norm(pnp)
+    else:
+        grid = make_virtual_grid(n_points=1000, device=device)
+        pnp = find_pnp(torch.concat((X_, Y_), dim=0), grid, device=device)
+
+    rot_axis, rot_angle = calc_rot(pnp, device=device)
+    rot_axis = rot_axis.to(device)
+    rot_angle = rot_angle.to(device)
+
+    X_ = rotate(X_, rot_axis, rot_angle)
+    Y_ = rotate(Y_, rot_axis, rot_angle)
+
+    X_sp = get_stereo_proj_torch(X_).to(device)
+    Y_sp = get_stereo_proj_torch(Y_).to(device)
+    s1_h = h(X_sp).double()
+    s2_h = h(Y_sp).double()
+
     projs = generate_rand_projs(s1_h.shape[-1], n_projs).to(device)
-    s1_h_rp, s2_h_rp = s1_h @ projs.T, s2_h @ projs.T    
+    s1_h_rp, s2_h_rp = s1_h @ projs.T, s2_h @ projs.T
 
     d = torch.abs(torch.sort(s1_h_rp.transpose(0, 1), dim=1).values - 
-            torch.sort(s2_h_rp.transpose(0, 1), dim=1).values)
+                  torch.sort(s2_h_rp.transpose(0, 1), dim=1).values)
 
     wd = d.pow(p).sum(dim=1).pow(1. / p).mean()
-    
     return wd
 
-def s3wd(samples_1, samples_2, p, h=None, n_projs=1000, n_iters=2000, lr=8e-3, eps=1e-2, device='cpu'):
-    if h is None:
-        h = Phi().to(device)  
-        
-    if n_iters == 0:
-        return s3wd0(samples_1, samples_2, p, h, n_projs=n_projs,device=device), h
-
-    h_optimizer = optim.Adam(h.parameters(), lr=lr)
-    samples_1_sp = get_stereo_proj_torch(samples_1).to(device)
-    samples_2_sp = get_stereo_proj_torch(samples_2).to(device)
-
-    wd = None  
-    loss0 = None
-
-    with tqdm(total=n_iters, desc='Optimizing h') as pbar:
-        for i in range(n_iters):
-            h_optimizer.zero_grad()
-            s1_h, s2_h = h(samples_1_sp).double(), h(samples_2_sp).double()
-
-            projs = generate_rand_projs(s1_h.shape[-1], n_projs).to(device)
-            s1_h_rp, s2_h_rp = s1_h @ projs.T, s2_h @ projs.T
-            swd = torch.abs(torch.sort(s1_h_rp.T, dim=1).values - torch.sort(s2_h_rp.T, dim=1).values)
-            swd = torch.sum(torch.pow(swd, p), dim=1)
-            swd = torch.pow(swd.mean() + 1e-12, 1. / p)
-
-            geod = torch.arccos(torch.clamp(torch.sum(samples_1 * samples_2, dim=-1), -1, 1))
-            loss = torch.mean(torch.abs(swd - geod))
-            if i == 0:
-                loss0 = loss.item()
-
-            if loss0 is not None and loss.item() < loss0 * eps:
-                pbar.update(n_iters - i) 
-                pbar.close()
-                wd = swd.item()  
-                break
-            
-            loss.backward()
-            h_optimizer.step()
-
-            pbar.set_postfix(loss=f"{loss.item():.4f}")
-            pbar.update(1)
-
-    if wd is None and swd is not None:  
-        wd = swd.item()
-
-    return wd, h
-
-def max_s3wd(samples_1, samples_2, n_projs, p=2, n_iters=1000, lam=20, device='cpu'):
-    h = Phi(samples_1.size(1)-1).to(device)
+def max_s3wd(X, Y, n_projs, p=2, n_iters=1000, lam=20, device='cpu'):
+    h = hPhi().to(device)
     h_optimizer = optim.Adam(h.parameters(), lr=8e-3, betas=(0.999, 0.999))
     
-    samples_1_sp = get_stereo_proj(samples_1.detach()).to(device)
-    samples_2_sp = get_stereo_proj(samples_2.detach()).to(device)
+    X_sp = get_stereo_proj(X.detach()).to(device)
+    Y_sp = get_stereo_proj(Y.detach()).to(device)
     
     for _ in range(n_iters):
         h_optimizer.zero_grad()
         
-        s1_h, s2_h = h(samples_1_sp).double(), h(samples_2_sp).double()
+        s1_h, s2_h = h(X_sp).double(), h(Y_sp).double()
         
         reg = lam * (s1_h.norm(p=2, dim=1) + s2_h.norm(p=2, dim=1)).mean()
         projs = generate_rand_projs(s1_h.shape[-1], n_projs).to(device)
@@ -99,7 +69,7 @@ def max_s3wd(samples_1, samples_2, n_projs, p=2, n_iters=1000, lam=20, device='c
         loss.backward(retain_graph=True)
         h_optimizer.step()
     
-    s1_h, s2_h = h(samples_1_sp), h(samples_2_sp)
+    s1_h, s2_h = h(X_sp), h(Y_sp)
     projs = generate_rand_projs(s1_h.shape[-1], n_projs).to(device)
     s1_h_rp, s2_h_rp = s1_h @ projs.T, s2_h @ projs.T
     d = torch.abs(torch.sort(s1_h_rp.T, dim=1).values - torch.sort(s2_h_rp.T, dim=1).values)
--- src/utils/s3w_utils.py
+++ src/utils/s3w_utils.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from tqdm import tqdm
 
 def get_stereo_proj(x):
     d = x.shape[-1] - 1
@@ -15,29 +16,140 @@ def get_stereo_proj_torch(x):
     d = x.shape[-1] - 1
     numerator = 2 * x[..., :d]
     denominator = 1 - x[..., d]
-    near_pole = torch.isclose(denominator, torch.zeros_like(denominator), rtol=1e-6, atol=1e-6)
+    near_pole = torch.isclose(denominator, torch.zeros_like(denominator), rtol=1e-9, atol=1e-9)
     proj = torch.full_like(x[..., :d], float('inf')) 
     proj[~near_pole] = numerator[~near_pole] / denominator[~near_pole].unsqueeze(-1)
     return proj
 
-# class Phi(nn.Module):
-#     def __init__(self, size):
-#         super(Phi, self).__init__()
-#         self.size = size
-#         self.net = nn.Sequential(nn.Linear(self.size, self.size))
-#     def forward(self, x):
-#         xhat = self.net(x)
-#         return torch.cat((x,xhat),dim=-1)
-    
+def rotate(points, axis, angle):
+    cos_ = torch.cos(angle)
+    sin_ = torch.sin(angle)
+    cross_prod = torch.cross(axis.repeat(points.shape[0], 1), points)
+    dot_prod = torch.sum(points * axis, dim=1, keepdim=True)
+    rotated_points = cos_.unsqueeze(-1) * points + sin_.unsqueeze(-1) * cross_prod + (1.0 - cos_).unsqueeze(-1) * dot_prod * axis
+    return rotated_points
+
+def calc_rot(new_pole, device='cpu'):
+    orig_pole = torch.tensor([0, 0, 1], dtype=torch.float32, device=device)
+    axis = torch.cross(orig_pole, new_pole)
+    axis = axis / torch.norm(axis) 
+    angle = torch.acos(torch.dot(orig_pole, new_pole) / torch.norm(new_pole))
+    return axis, angle
+
+def make_virtual_grid(n_points=1000, device='cpu'):
+    phi = torch.rand(n_points, device=device) * 2 * torch.pi
+    cos_theta = torch.rand(n_points, device=device) * 2 - 1
+    theta = torch.acos(cos_theta)
+
+    x = torch.sin(theta) * torch.cos(phi)
+    y = torch.sin(theta) * torch.sin(phi)
+    z = torch.cos(theta)
+
+    return torch.stack([x, y, z], dim=1)
+
+def find_pnp(X, grid, device='cuda'):
+    X = X.to(device)
+    grid = grid.to(device)
+    D= torch.cdist(X, grid)
+    min_ds, _ = torch.min(D, dim=0)
+    idx = torch.argmax(min_ds).item()
+    return grid[idx]
+
+
 class Phi(nn.Module):
-    def __init__(self, hidden_dim=128):
-        super().__init__()
-        self.linear1 = nn.Linear(2, hidden_dim)
-        self.linear2 = nn.Linear(hidden_dim, hidden_dim)
-        self.linear3 = nn.Linear(hidden_dim, 2)
+    def __init__(self, size):
+        super(Phi, self).__init__()
+        self.size = size
+        self.net = nn.Sequential(nn.Linear(self.size, self.size))
+    def forward(self, x):
+        xhat = self.net(x)
+        return torch.cat((x,xhat),dim=-1)
+    
+
+class hStar(nn.Module):
+    def __init__(self):
+        super(hStar, self).__init__()
 
     def forward(self, x):
-        xhat = nn.Tanh()(self.linear1(x))
-        xhat = nn.Tanh()(self.linear2(xhat))
-        xhat = self.linear3(xhat)
-        return torch.cat((xhat, x), dim=1) 
+        x_sq_sum = (x**2).sum(1).clamp(min=1e-8)
+        x_dp1 = torch.clip((x_sq_sum - 1) / (x_sq_sum + 1), -1, 1)
+        arc_input = torch.clip(-x_dp1, -1, 1)
+        arccos = torch.arccos(arc_input)
+        h = (arccos / np.pi).unsqueeze(1) * (x / torch.sqrt(x_sq_sum).unsqueeze(1))
+        return h
+
+class MLP(nn.Module):
+    def __init__(self,architecture=[1,256,128,2],activation=nn.LeakyReLU()):
+        super(MLP, self).__init__()
+        self.activation = activation
+        self.architecture = architecture
+        arch = []
+        for i in range(1, len(architecture)-1):
+            arch.append(nn.Linear(architecture[i-1], architecture[i]))
+            arch.append(self.activation)
+        self.basis = nn.Sequential(*arch)
+        self.regressor = nn.Linear(architecture[-2], architecture[-1])
+
+    def forward(self,x):
+        assert x.shape[1] == self.architecture[0]
+        z = self.basis(x)
+        out = self.regressor(z)
+        return out
+    
+class FF(nn.Module):
+    def __init__(self,din=3,dff=256,w0=30):
+        super(FF, self).__init__()
+        self.B = nn.Parameter(torch.randn(din, dff), requires_grad=False)
+        self.w0 = w0
+        self.dff_ = dff
+        self.din_ = din
+    def forward(self,x):
+        z = x @ self.B
+        return torch.cat([torch.sin(self.w0 * z), torch.cos(self.w0 * z)],1)
+    
+class hPhi(nn.Module):
+    def __init__(self, input_dim=2, fourier_dff=512, mlp_architecture=[1024, 1024, 32], w0=30, device='cpu'):
+        super(hPhi, self).__init__()
+        self.FF = FF(din=input_dim, dff=fourier_dff, w0=w0)
+        self.mlp = MLP(architecture=[fourier_dff * 2] + mlp_architecture)
+        self.model=nn.Sequential(self.FF,self.mlp).to(device)
+        self.device = device
+        self.to(device)
+
+    def forward(self, x):
+        return self.model(x)
+
+    def train(self, epochs, batchsize, lr=1e-4):
+        optimizer = torch.optim.AdamW(self.model.parameters(), lr=lr)
+        lossE = []
+        lossAvg = []
+        
+        pbar = tqdm(range(epochs), total=epochs)
+        for epoch in pbar:
+            z = torch.randn(batchsize, 3).to(self.device)
+            z = z / (torch.norm(z, dim=1, keepdim=True))
+
+            inner = (z.unsqueeze(1) * z.unsqueeze(0)).sum(2)
+            inner = torch.clip(inner, -1, 1)
+            arclengths_sq = (torch.arccos(inner))**2
+
+            x = get_stereo_proj_torch(z)
+            h = self(x)
+            euclidean_sq = ((h.unsqueeze(1) - h.unsqueeze(0))**2).sum(2)
+            if torch.isnan(euclidean_sq).any():
+                print(f"NaN at epoch {epoch}")
+                break
+            loss = torch.mean((arclengths_sq - euclidean_sq)**2)
+
+            optimizer.zero_grad()
+            loss.backward()
+            # torch.nn.utils.clip_grad_norm_(self.parameters(), max_norm=1.0)
+            optimizer.step()
+            lossE.append(loss.item())
+            
+            pbar.set_description(f'Epoch {epoch+1}/{epochs}, Loss: {loss.item():.4f}')
+            if np.mod(epoch, 1000) == 0 and epoch != 0:
+                lossAvg.append(np.mean(lossE))
+                lossE = []
+
+        print(f'Done. Final loss: {lossE[-1]}')
