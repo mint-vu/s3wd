d106eeec611f3f07161e2bc3d09d30fb25457f55
Huy Tran <continuousml@gmail.com>
2024-01-21 18:35:50 -0500

    add armotized option

--- src/methods/s3w.py
+++ src/methods/s3w.py
@@ -9,16 +9,53 @@ from utils.s3w_utils import get_stereo_proj_torch, epsilon_projection, Phi, hSta
 
 from typing import List
 
-def ri_s3wd(X, Y, p, h=None, n_projs=1000, n_rotations=1, device='cpu'):
-    # NOTE: h must accept vectors of the form (n_rotations, n_points, dim)
+# def ri_s3wd(X, Y, p, h=None, n_projs=1000, n_rotations=1, device='cpu'):
+#     # NOTE: h must accept vectors of the form (n_rotations, n_points, dim)
+#     if h is None: h = hStar()
+    
+#     X = X.to(device)
+#     Y = Y.to(device)
+    
+#     n = X.shape[-1]
+#     rot_matrices = [geotorch.SO(torch.Size([n, n])).sample('uniform') for _ in range(n_rotations)]
+#     rot_matrices = torch.stack(rot_matrices).to(device)
+#     X_rot = (rot_matrices @ X.T).permute(0, 2, 1)
+#     Y_rot = (rot_matrices @ Y.T).permute(0, 2, 1)
+
+#     X_eps = epsilon_projection(X_rot)
+#     Y_eps = epsilon_projection(Y_rot)
+
+#     X_sp = get_stereo_proj_torch(X_eps).to(device)
+#     Y_sp = get_stereo_proj_torch(Y_eps).to(device)
+
+#     s1_h = h(X_sp).double()
+#     s2_h = h(Y_sp).double()
+
+#     projs = generate_rand_projs(s1_h.shape[-1], n_projs).to(device)
+#     s1_h_rp, s2_h_rp = s1_h @ projs.T, s2_h @ projs.T
+
+#     d = torch.abs(torch.sort(s1_h_rp.transpose(-2, -1), dim=-1).values - 
+#                 torch.sort(s2_h_rp.transpose(-2, -1), dim=-1).values)
+
+#     wd = d.pow(p).sum(dim=-1).pow(1. / p).mean(dim=-1)
+#     return wd.mean()
+
+def ri_s3wd(X, Y, p, h=None, n_projs=1000, n_rotations=1, R=None, device='cpu'):
+    """armotized option"""
     if h is None: h = hStar()
+    
+    if R is not None:
+        assert n_rotations <= R.size(0)
+        indices = torch.randperm(R.size(0))[:n_rotations]
+        rot_matrices = R[indices].to(device)
+    else:
+        n = X.shape[-1]
+        rot_matrices = [geotorch.SO(torch.Size([n, n])).sample('uniform') for _ in range(n_rotations)]
+        rot_matrices = torch.stack(rot_matrices).to(device)
     
     X = X.to(device)
     Y = Y.to(device)
     
-    n = X.shape[-1]
-    rot_matrices = [geotorch.SO(torch.Size([n, n])).sample('uniform') for _ in range(n_rotations)]
-    rot_matrices = torch.stack(rot_matrices).to(device)
     X_rot = (rot_matrices @ X.T).permute(0, 2, 1)
     Y_rot = (rot_matrices @ Y.T).permute(0, 2, 1)
 
@@ -35,7 +72,7 @@ def ri_s3wd(X, Y, p, h=None, n_projs=1000, n_rotations=1, device='cpu'):
     s1_h_rp, s2_h_rp = s1_h @ projs.T, s2_h @ projs.T
 
     d = torch.abs(torch.sort(s1_h_rp.transpose(-2, -1), dim=-1).values - 
-                torch.sort(s2_h_rp.transpose(-2, -1), dim=-1).values)
+                  torch.sort(s2_h_rp.transpose(-2, -1), dim=-1).values)
 
     wd = d.pow(p).sum(dim=-1).pow(1. / p).mean(dim=-1)
     return wd.mean()
