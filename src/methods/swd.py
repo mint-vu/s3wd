65f33a6da30abbb8fda7772d0ffc71eb525f1d0d
Huy Tran <continuousml@gmail.com>
2024-06-03 01:47:33 -0400

    add vsw

--- src/methods/swd.py
+++ src/methods/swd.py
@@ -1,12 +1,20 @@
 import torch
-from utils.misc import generate_rand_projs
+from utils.misc import generate_rand_projs, generate_equator_projs
 
-def swd(samples_1, samples_2, p, n_projs=200, device='cpu'):
-    projs = generate_rand_projs(samples_1.shape[-1], n_projs).to(device)
-    s1_proj, s2_proj = samples_1.double() @ projs.T, samples_2.double() @ projs.T
+def swd(X, Y, p, n_projs=200, device='cpu'):
+    projs = generate_rand_projs(X.shape[-1], n_projs).to(device)
+    s1_proj, s2_proj = X.double() @ projs.T, Y.double() @ projs.T
 
     d = torch.abs(torch.sort(s1_proj.transpose(0, 1), dim=1).values - 
                   torch.sort(s2_proj.transpose(0, 1), dim=1).values)
 
     wd = d.pow(p).sum(dim=1).mean()
     return wd
+
+def vswd(X, Y, p, n_projs=200, device='cpu'):
+    projs = generate_equator_projs(X.shape[-1], n_projs).to(device)
+    s1_proj, s2_proj = X.double() @ projs.T, Y.double() @ projs.T
+    d = torch.abs(torch.sort(s1_proj.transpose(0, 1), dim=1).values -
+                  torch.sort(s2_proj.transpose(0, 1), dim=1).values)
+    vswd = d.pow(p).sum(dim=1).mean()
+    return vswd
